31637bb48f4652f2d3313b3521dfcd309d154105
pashbylogan <pashbyl@wwu.edu>
2020-05-13 15:12:59 -0500

    first draft custom dataset

--- dataset.py
+++ dataset.py
@@ -6,13 +6,17 @@ Custom dataset class to feed to pytorch dataloader for train/dev picture files.
 """
 
 import torch
-from torch.utils import data
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms, utils
 import numpy as np
+import random
 import os
+from PIL import Image
 from skimage import io
 import csv
+import randomTransformation
 
-class FlukeDataset(data.Dataset):
+class FlukeDataset(Dataset):
     def __init__(self, filepath):
         'Initialization'
         self.inputs = []
@@ -30,9 +34,33 @@ class FlukeDataset(data.Dataset):
 
     def __getitem__(self, index):
         'Generates one sample of data'
+        
         file = os.path.join(self.filepath, self.inputs[index])
-
-        x = io.read(file)
+        x = applyRandomTransformations(torch.from_numpy(io.read(file)))
         y = self.targets[index]
 
         return x, y
+
+    def applyRandomTransformations(self, iTensor):
+        rList = [1, 1, 1, 2, 2, 3] #number of transformations to be made
+        rChoice = random.choice(rList)
+        tList = [1, 2, 3, 4, 5, 6] #possible transformation possibilities
+        
+        transformList = []
+        
+        for x in range(rChoice):
+            tChoice = random.choice(tList) #picks a random transformation
+            if (tChoice == 1): #converts to greyscale
+                transformList.append(RandomGrayscale(p=random.uniform(0.2, 1)))
+            elif (tChoice == 2): #horizantal flip
+                transformList.append(RandomHorizontalFlip(p=1))
+            elif (tChoice == 3): #vertical flip
+                transformList.append(RandomVerticalFlip(p=1))
+            elif (tChoice == 4): #random rotation
+                transformList.append(RandomRotation((-359, 359), resample=False,expand=True, center=None))
+            elif (tChoice == 5): #shifts and wraps
+                transformList.append(RandomCrop((randint(iTensor.size/4, iTensor.size), randint(iTensor.size/4, iTensor.size)), scale=(0.08, 1.0), ratio=(0.75, 1.3333333333333333), interpolation=2))
+            else: #resizes the array
+                transformList.append(Resize(randint(iTensor.size/2, iTensor.size*2)))
+                
+        return transforms.Compose(transformList) 
