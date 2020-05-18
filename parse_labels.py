bd620d6f51992973c399912ea91292437edb1276
Connor Barlow <conn.barlow.r@gmail.com>
2020-05-17 21:50:04 -0700

    Added balanced class-splitting functionality to parse_labels
    Added class splitting option to parse_labels and generated a new set of labels using the default settings.
    This conforms better to a subtle point of prototypical networks that was not clear before. Performance and actual
    rationale are pending, but it seems to provide more order in this ludacris dataset

--- parse_labels.py
+++ parse_labels.py
@@ -1,5 +1,6 @@
 import argparse
 import csv
+import math
 from random import shuffle
 
 """An extremely simple logger that just provides a 
@@ -19,27 +20,87 @@ class Logger():
      train and dev sets, filtered accordingly."""
 def parse_labels(logger, args):
     with open(args.input_path, "r", newline='') as labels:
+        # Read csv and get list of filtered rows
         labels = csv.DictReader(labels)
-        filtered = filter_labels(logger, labels, args)
+        filtered = filter_labels(labels, logger, args)
 
-        shuffle(filtered)
-        split = int(args.dev_split * len(filtered))
+        # Split data into two seperate lists of rows
+        (dev, train) = (None, None)
+        if (args.class_split):
+            (dev, train) = class_split(filtered, logger, args)
+        else:
+            (dev, train) = data_split(filtered, logger, args)
+        
+        logger.log("Adding data points...")
+        # Output the rows, maintaing the csv's format
         with open(args.dev_output, "w", newline='') as dev_out:
             dev_out = csv.DictWriter(dev_out, fieldnames=labels.fieldnames)
             dev_out.writeheader()
-            dev_out.writerows(filtered[:split])
-            logger.log(str(split) + " added to dev set")
+            for row in dev:
+                dev_out.writerow(row)
+            logger.log("\t"+str(len(dev)) + " added to dev set")
 
         with open(args.train_output, "w", newline='') as train_out:
             train_out = csv.DictWriter(train_out, fieldnames=labels.fieldnames)
             train_out.writeheader()
-            train_out.writerows(filtered[split:])
-            logger.log(str(len(filtered) - split + 1) + " added to train set")
+            for row in train:
+                train_out.writerow(row)
+            logger.log("\t"+str(len(train)) + " added to train set")
+        
+def data_split(filtered, logger, args):
+    shuffle(filtered)
+    split = int(args.dev_split * len(filtered))
+    return (filtered[:split], filtered[split:])
 
+def class_split(filtered, logger, args):
+    # Count examples per class for all classes
+    # It also has to contain the rows and my god this is dumb
+    class_num = {}
+    for row in filtered:
+        if row['Id'] in class_num:
+            class_num[row['Id']].append(row)
+        else:
+            class_num[row['Id']] = [row]
+
+    # Reverse! Reverse!
+    num_class = {}
+    for k in list(class_num):
+        num = len(class_num[k])
+        if num in num_class:
+            num_class[num].append(class_num[k])
+        else:
+            num_class[num] = [class_num[k]]
+    
+    # Cha cha real smooth
+    logger.log("Splitting classes...")
+    (dev, train) = ([], [])
+    ceil = True
+    for num in sorted(list(num_class)):
+        split = None
+        # Switch between floor/ceiling resolutions of class splitting, 
+        # More necessary when the number of classes being split is around 1
+        if ceil:
+            split = math.ceil(args.dev_split * len(num_class[num]))
+            ceil = False
+        else:
+            split = math.floor(args.dev_split * len(num_class[num]))
+            ceil = True
+        
+        for full_class in num_class[num][:split]:
+            dev.extend(full_class)
+        for full_class in num_class[num][split:]:
+            train.extend(full_class)
+        
+        logger.log("\tNumber of classes with " + str(num) + " examples, " +
+            "dev=" + str(split) + ",train=" + str(len(num_class[num][split:])))
+
+    # Lets go to work!
+    logger.log("")
+    return (dev,train)
 
 """Filter labels out using the args, and returns a list 
      of "rows" (just dicts) for further processing"""
-def filter_labels(logger, labels, args):
+def filter_labels(labels, logger, args):
     filtered = []
 
     new_whale_skipped = 0
@@ -67,6 +128,7 @@ def filter_labels(logger, labels, args):
     logger.log("\tAR_UT: " + str(AR_UT_skipped))
     logger.log("Total skips: " + str(new_whale_skipped + AR_LT_skipped + AR_UT_skipped))
     logger.log("Remaining data points: " + str(len(filtered)))
+    logger.log("")
     return filtered
 
 
@@ -77,6 +139,10 @@ def parse_args():
     parser.add_argument("train_output", help="The path to the output training CSV")
     parser.add_argument("dev_output", help="The path to the output development CSV")
 
+    parser.add_argument("-class_split", type=bool,
+        help="Whether split is done per-data point or per-class. \
+            If per class, the splits try and keep an even distrabution of examples-per-class \
+            per class per train/dev dataset relative to dev_split (bool) [default: True]", default=True)
     parser.add_argument("-dev_split", type=float, 
         help="The proportion of data for dev set (float) [default: 0.2]", default=0.2)
     parser.add_argument("-new_whale", type=bool,
