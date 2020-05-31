c7f3d1dfd135dc5e2c2816bfde658bc3c70a0733
Connor Barlow <conn.barlow.r@gmail.com>
2020-05-30 17:22:04 -0700

    Test set & extendable train/dev/test reporting
    Added the reporting system that will probably be good for the rest of this project
    Test set is very basic, and now when model.py is run the full training sequence happens

--- utils.py
+++ utils.py
@@ -1,9 +1,12 @@
+import os
 import csv
-from sklearn.manifold import TSNE
+from statistics import mean, stdev
+#from sklearn.manifold import TSNE
 import numpy as np
 import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
+
 
 def calc_AP_5(class_predictions, true_class):
     """
@@ -70,3 +73,75 @@ def visualize_embeddings(embeddings,labels,s):
 
     plt.margins(0.1)
     plt.savefig("%s.pdf" % s)
+
+# Recorder that just stores the episode and accuracy
+class PerformanceRecord():
+    def __init__(self, name, out_path, dbg=False):
+        self.dbg = dbg
+        self.name = name
+
+        # Create file, making sure we don't override the old stuff
+        files = [f for f in os.listdir(out_path) if os.path.isfile(os.path.join(out_path, f))]
+        suffix=".csv"
+        i=-1
+        while name+suffix in files:
+            i+=1
+            suffix="_"+str(i)+".csv"
+
+        path = os.path.join(out_path,name+suffix)
+
+        # Create csv writter
+        self.fieldnames = ["episode", "acc"]
+        self.eval_file = open(path, "w", newline='')
+        self.eval_file = csv.DictWriter(self.eval_file, fieldnames=self.fieldnames)
+        self.eval_file.writeheader()
+    
+    def write_record(self, episode, acc):
+        row = {}
+        row["episode"] = episode
+        row["acc"] = acc
+        if self.dbg:
+            print("{}: episode {}, acc {:.4f}".format(self.name, row["episode"], row["acc"]))
+        self.eval_file.writerow(row)
+
+# Variation of PerformanceRecord that is more useful for multi-epoch runs
+class AggregatePerformanceRecord():
+    def __init__(self, name, out_path, dbg=False):
+        self.dbg = dbg
+        self.name = name
+
+        # Create file, making sure we don't override the old stuff
+        files = [f for f in os.listdir(out_path) if os.path.isfile(os.path.join(out_path, f))]
+        suffix=".csv"
+        i=-1
+        while name+suffix in files:
+            i+=1
+            suffix="_"+str(i)+".csv"
+
+        path = os.path.join(out_path,name+suffix)
+
+        # Create csv writter
+        self.fieldnames = ["curr_epoch", "episodes", "avg_acc", "std_dev"]
+        self.eval_file = open(path, "w", newline='')
+        self.eval_file = csv.DictWriter(self.eval_file, fieldnames=self.fieldnames)
+        self.eval_file.writeheader()
+
+        self.buffer = []
+
+    def __len__(self):
+        return len(self.buffer)
+
+    def add_record(self, acc):
+        self.buffer.append(acc)
+
+    def write_record_buffer(self, curr_epoch):
+        row = {}
+        row["curr_epoch"] = curr_epoch
+        row["episodes"] = len(self.buffer)
+        row["avg_acc"] = mean(self.buffer)
+        row["std_dev"] = stdev(self.buffer)
+        self.eval_file.writerow(row)
+        if self.dbg:
+            print("{}: curr_epoch {:.2f}, episodes {}, avg_acc {:.4f}, std_dev {:.4f}".format(self.name, row["curr_epoch"], row["episodes"], row["avg_acc"], row["std_dev"]))
+        self.buffer = []
+
